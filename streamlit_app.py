7a9af5c6ab10fe37a097b012c33048e207e76ab8
quosu <mnklosowski@gmail.com>
2022-09-15 13:58:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthly Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled range-free egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍵 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍲 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥚 Hard-boiled range-free egg')
+streamlit.text('🥑 Avocado Toast')

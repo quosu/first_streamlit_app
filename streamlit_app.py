7c3d40efae2c50a56c722c9f6ff02e1862f55a7f
quosu <mnklosowski@gmail.com>
2022-09-15 13:51:49 +0200

    Update streamlit_app.py
    c2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthly Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-boiled range-free egg')

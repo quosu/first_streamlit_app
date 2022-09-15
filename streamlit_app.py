56494d9abc25c126ccbba7415aff1620bb49b5eb
quosu <mnklosowski@gmail.com>
2022-09-15 13:48:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthly Dinner')
 
-streamlit.head('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
